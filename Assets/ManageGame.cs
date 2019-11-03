5b3800f51010e2cc2cd86d75fe3b58717985c379
Elena B <elena@ardeaarts.org>
2019-11-03 16:35:34 -0500

    Initial commit

---
+++ Assets/ManageGame.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ManageGame : MonoBehaviour {
+
+    // Elena wrote this to manage the opening screens of the game. This creates screen groups and
+    // makes them visible as needed
+
+    [SerializeField]  // this allows a var to appear in the inspector even though it is private
+    private CanvasGroup titleScreen;
+
+    [SerializeField]
+    private CanvasGroup inputName;
+
+    [SerializeField]
+    private CanvasGroup instructPlayer;
+
+    public Text tellPlayer;
+    public InputField playerInput;
+    public string playerName;
+
+
+
+    void Awake() // when game starts, make title screen active and turn off the input name and instruct screens
+    {
+        inputName.alpha = 0f;
+        inputName.interactable = false;
+        inputName.blocksRaycasts = false;
+
+        instructPlayer.alpha = 0f;
+        instructPlayer.interactable = false;
+        instructPlayer.blocksRaycasts = false;
+    }
+
+    public void LoadInputName() // when this function is called, turn off title screen and turn on input
+    {
+
+        inputName.alpha = 1f;
+        inputName.interactable = true;
+        inputName.blocksRaycasts = true;
+
+        titleScreen.alpha = 0f;
+        titleScreen.interactable = false;
+        titleScreen.blocksRaycasts = false;
+
+    }
+
+
+
+    public void TeachPlayer() //when this function is called, turn off input screen and turn on instruct
+    {
+
+        inputName.alpha = 0f;
+        inputName.interactable = false;
+        inputName.blocksRaycasts = false;
+
+        instructPlayer.alpha = 1f;
+        instructPlayer.interactable = true;
+        instructPlayer.blocksRaycasts = true;
+
+    }
+
+    public void TellPlayerHi()
+    {
+        playerName = playerInput.text;
+        Debug.Log("name of player is: " + playerName);
+        tellPlayer.text = "Hi " + playerName + " , let's ruffle some feathers";
+
+    }
+
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene("GameLevelOne");
+        PlayerPrefs.SetString("ThisPlayer", playerName);
+    }
+
+}
+
---
+++ Assets/ManageLevels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageLevels : MonoBehaviour
+{
+    public Text greetPlayer;
+    public string gamePlayer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gamePlayer = PlayerPrefs.GetString("ThisPlayer");
+        greetPlayer.text = "Hello " + gamePlayer;
+    }
+}
